095464418e6ac7d3c47753670bec28186a61e25f
elifozer <ozere@itu.edu.tr>
2017-11-18 06:10:20 +0300

    Added a folder for user data base object

---
+++ elifozer_dbmodels/userdbo.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self):
+        self.userId = -1
+        self.firstName = ""
+        self.lastName = ""
+        self.username = ""
+        self.password = ""
+        self.email = ""
