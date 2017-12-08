06de530e8d4b4420f5b458b78d71a04e4f3c405d
elifozer <ozere@itu.edu.tr>
2017-12-08 18:15:24 +0300

    Select, insert, update and delete queries for musician table are applied. Select method can be done using the id and the musician name.

---
+++ elifozer_dbhandler/musicianhandler.py
@@ -0,0 +1,95 @@
+from elifozer_dbhandler import basehandler
+from elifozer_utilities.filterparameter import FilterParameter
+from elifozer_utilities.filterexpression import FilterExpression
+from elifozer_dbmodels.musiciandbo import Musician
+
+
+def GetByID(musicianId):
+    filterParameter = FilterParameter("MUSICIANID" , "=", musicianId)
+    filterExpression = FilterExpression()
+    filterExpression.AddParameter(filterParameter)
+
+    return Get(filterExpression)[0]
+
+
+def GetByMusicianName(musicianName):
+    filterParameter = FilterParameter("MUSICIANNAME" , "LIKE", musicianName)
+    filterExpression = FilterExpression()
+    filterExpression.AddParameter(filterParameter)
+
+    musicianList = Get(filterExpression)
+
+    if len(musicianList) > 0:
+        return musicianList[0]
+
+    return Musician()
+
+
+def Get(filterExpression = None):
+    connection, cursor = basehandler.DbConnect()
+
+    myQuery = "SELECT * FROM MUSICIAN_DBT"
+
+    if filterExpression is None:
+        cursor = basehandler.DbExecute(myQuery, connection, cursor)
+    else:
+        myQuery += filterExpression.GetWhere()
+        cursor = basehandler.DbExecute(myQuery, connection, cursor, filterExpression.GetParameters())
+
+    musicianList = []
+
+    for musician in cursor.fetchall():
+        tempMusician = Musician()
+
+        tempMusician.musicianId = musician[0]
+        tempMusician.name = musician[1]
+        tempMusician.genre = musician[2]
+        tempMusician.establishYear = musician[3]
+
+        musicianList.append(tempMusician)
+
+    basehandler.DbClose(connection, cursor)
+
+    return musicianList
+
+
+def Insert(newMusician):
+    connection, cursor = basehandler.DbConnect()
+
+    myQuery = """INSERT INTO MUSICIAN_DBT(MUSICIANNAME, MUSICIANGENRE, MUSICIANESTYEAR)
+                 VALUES (%s, %s, %s) RETURNING MUSICIANID;"""
+
+    cursor = basehandler.DbExecute(myQuery, connection, cursor, (newMusician.name, newMusician.genre, newMusician.establishYear))
+
+    newMusician.musicianId = cursor.fetchone()[0]
+
+    basehandler.DbClose(connection, cursor)
+
+    return newMusician
+
+
+def Update(currentMusician):
+    connection, cursor = basehandler.DbConnect()
+
+    myQuery = """UPDATE MUSICIAN_DBT SET MUSICIANNAME = %s,
+                                         MUSICIANGENRE = %s,
+                                         MUSICIANESTYEAR = %s,
+                 WHERE MUSICIANID = %s"""
+
+    cursor = basehandler.DbExecute(myQuery, connection, cursor, (currentMusician.name, currentMusician.genre, currentMusician.establishYear))
+
+    basehandler.DbClose(connection, cursor)
+
+    return currentMusician
+
+
+def Delete(musicianId):
+    connection, cursor = basehandler.DbConnect()
+
+    myQuery = "DELETE FROM MUSICIAN_DBT WHERE MUSICIANID = " + str(musicianId)
+
+    cursor = basehandler.DbExecute(myQuery, connection, cursor)
+
+    basehandler.DbClose(connection, cursor)
+
+    return True
